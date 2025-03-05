675fe0eefe3d8ee667f5845e6f1e80b8ed1f41fd
Cavernos <louisdescavernes@gmail.com>
2025-03-05 21:32:23 +0100

    Modify JavaScript to add block class

--- src/app/robomaster_extension.js
+++ src/app/robomaster_extension.js
@@ -1,77 +1,21 @@
-class CustomExtension {
-    constructor(runtime) {
-        this.runtime = runtime;
+class Block {
+    arguments = {
+        SERVE_METHOD: "GET"
     }
-
-    getInfo() {
+    constructor(opcode, blockType, text, args = null) {
+        this.opcode = opcode
+        this.blockType = blockType
+        this.text = text
+        this.arguments = args
+    }
+    toJSON(){
         return {
-            id: 'customExtension',
-            name: 'Robomaster Extension',
-            color1: '#4C97FF',
-            color2: '#3373CC',
-            blocks: [
-                {
-                    opcode: 'start',
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: 'Start'
-                },
-                {
-                    opcode: 'stop',
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: 'Stop'
-                },
-                {
-                    opcode: 'move',
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: 'Move x: [x] y: [y] z: [z] speed: [speed]',
-                    arguments: {
-                        x: { type: Scratch.ArgumentType.NUMBER, defaultValue: 1 },
-                        y: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 },
-                        z: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 },
-                        speed: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0.5 }
-                    }
-                },
-                {
-                    opcode: 'rotate',
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: 'Rotate angle: [angle]',
-                    arguments: {
-                        angle: { type: Scratch.ArgumentType.NUMBER, defaultValue: 90 }
-                    }
-                },
-                {
-                    opcode: 'gimbal',
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: 'Move gimbal pitch: [pitch] yaw: [yaw]',
-                    arguments: {
-                        pitch: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 },
-                        yaw: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 }
-                    }
-                },
-                {
-                    opcode: 'arm',
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: 'Move arm to [position]',
-                    arguments: {
-                        position: { type: Scratch.ArgumentType.STRING, menu: 'armPositions' }
-                    }
-                },
-                {
-                    opcode: 'grabber',
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: 'Grabber [action]',
-                    arguments: {
-                        action: { type: Scratch.ArgumentType.STRING, menu: 'grabberActions' }
-                    }
-                }
-            ],
-            menus: {
-                armPositions: ['up', 'down'],
-                grabberActions: ['open', 'close']
-            }
-        };
+            opcode: this.opcode,
+            blockType: this.blockType,
+            text: this.text,
+            arguments: this.arguments
+        }
     }
-
     async requestHandler(url, method = 'POST', body = null) {
         try {
             const options = { method, headers: { 'Content-Type': 'application/json' } };
@@ -82,6 +26,82 @@ class CustomExtension {
             console.error(`Erreur lors de la requête ${url}:`, error);
         }
     }
+    async run(method, args){
+        await this.requestHandler(this.opcode, this.arguments.SERVE_METHOD, args)
+    }
+
+}
+
+class CustomExtension {
+    constructor(runtime) {
+        this.runtime = runtime;
+        this.blocks = [
+            (new Block( 'start', Scratch.BlockType.COMMAND, 'Start')).toJSON(),
+            (new Block('stop',  Scratch.BlockType.COMMAND,'Stop')).toJSON(),
+        ]
+    }
+
+    getInfo() {
+        return {
+            id: 'customExtension',
+            name: 'Robomaster Extension',
+            color1: '#4C97FF',
+            color2: '#3373CC',
+            blocks: this.blocks
+        //         [
+        //         {
+        //             opcode: 'move',
+        //             blockType: Scratch.BlockType.COMMAND,
+        //             text: 'Move x: [x] y: [y] z: [z] speed: [speed]',
+        //             arguments: {
+        //                 x: { type: Scratch.ArgumentType.NUMBER, defaultValue: 1 },
+        //                 y: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 },
+        //                 z: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 },
+        //                 speed: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0.5 }
+        //             }
+        //         },
+        //         {
+        //             opcode: 'rotate',
+        //             blockType: Scratch.BlockType.COMMAND,
+        //             text: 'Rotate angle: [angle]',
+        //             arguments: {
+        //                 angle: { type: Scratch.ArgumentType.NUMBER, defaultValue: 90 }
+        //             }
+        //         },
+        //         {
+        //             opcode: 'gimbal',
+        //             blockType: Scratch.BlockType.COMMAND,
+        //             text: 'Move gimbal pitch: [pitch] yaw: [yaw]',
+        //             arguments: {
+        //                 pitch: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 },
+        //                 yaw: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 }
+        //             }
+        //         },
+        //         {
+        //             opcode: 'arm',
+        //             blockType: Scratch.BlockType.COMMAND,
+        //             text: 'Move arm to [position]',
+        //             arguments: {
+        //                 position: { type: Scratch.ArgumentType.STRING, menu: 'armPositions' }
+        //             }
+        //         },
+        //         {
+        //             opcode: 'grabber',
+        //             blockType: Scratch.BlockType.COMMAND,
+        //             text: 'Grabber [action]',
+        //             arguments: {
+        //                 action: { type: Scratch.ArgumentType.STRING, menu: 'grabberActions' }
+        //             }
+        //         }
+        //     ],
+        //     menus: {
+        //         armPositions: ['up', 'down'],
+        //         grabberActions: ['open', 'close']
+        //     }
+         };
+    }
+
+
 
     async start() {
         await this.requestHandler('start');

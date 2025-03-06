699f7b13b4d862b0e7729298c458dbf20fcb1f87
Nicolas <nico21.clement@gmail.com>
2025-03-07 00:08:00 +0100

    Preparation for implementation robomaster_serveur.py LED Effects, Chassis, Armor, Media

--- src/app/robomaster_server.py
+++ src/app/robomaster_server.py
@@ -51,7 +51,6 @@ class RoboMasterServer:
     def index(self):
         return jsonify({"api": "ok !"})
 
-
     def get_robot_ip(self):
         return self.safe_execute(self._get_robot_ip, "No Robot in the same LAN")
 
@@ -93,6 +92,9 @@ class RoboMasterServer:
         
     # -------------------- BLOC FUNCTIONS -------------------- #
 
+    # Robomaster Basics
+    
+    # start
     def start(self):
         """
         Initialize connection with the RoboMaster robot.
@@ -110,6 +112,7 @@ class RoboMasterServer:
         self.ep_robot.initialize(conn_type="ap")
         return jsonify({"start": True})
     
+    # stop
     def stop(self):
         """
         Stop the connection with the RoboMaster robot.
@@ -126,10 +129,94 @@ class RoboMasterServer:
         self.ep_robot.close()
         return jsonify({"stop": True})
     
+    
+    # LED Effects
+    
+    # set_flash
+    def set_flash(self):
+        return self.safe_execute(self._set_flash, "Failed to set flash")
+    
+    def _set_flash(self):
+        return jsonify({"set_flash": True})
+
+    # set_bottom_led
+    def set_bottom_led(self):
+        return self.safe_execute(self._set_bottom_led, "Failed to set bottom flash")
+    
+    def _set_bottom_led(self):
+        return jsonify({"set_bottom_led": True})
+    
+    # set_top_led
+    def set_top_led(self):
+        return self.safe_execute(self._set_top_led, "Failed to set top flash")
+    
+    def _set_top_led(self):
+        return jsonify({"set_top_led": True})
+    
+    # set_signle_led
+    def set_signle_led(self):
+        return self.safe_execute(self._set_signle_led, "Failed to set signle led")
+    
+    def _set_signle_led(self):
+        return jsonify({"set_signle_led": True})
+    
+    # turn_off
+    def turn_off(self):
+        return self.safe_execute(self._turn_off, "Failed to turn off")
+    
+    def _turn_off(self):
+        return jsonify({"turn_off": True})
+        
+    
+    # Chassis
+    
+    # set_pwm_value
+    def set_pwm_value(self):
+        return self.safe_execute(self._set_pwm_value, "Failed to set pwm value")
+    
+    def _set_pwm_value(self):
+        return jsonify({"set_pwm_value": True})
+    
+    # enable_stick_overlay
+    def enable_stick_overlay(self):
+        return self.safe_execute(self._enable_stick_overlay, "Failed to turn off")
+    
+    def _enable_stick_overlay(self):
+        return jsonify({"enable_stick_overlay": True})
+    
+    # set_follow_gimbal_offset
+    def set_follow_gimbal_offset(self):
+        return self.safe_execute(self._set_follow_gimbal_offset, "Failed to turn off")
+    
+    def _set_follow_gimbal_offset(self):
+        return jsonify({"set_follow_gimbal_offset": True})
+    
+    # set_trans_speed
+    def set_trans_speed(self):
+        return self.safe_execute(self._set_trans_speed, "Failed to turn off")
+    
+    def _set_trans_speed(self):
+        return jsonify({"set_trans_speed": True})
+    
+    # set_rotate_speed
+    def set_rotate_speed(self):
+        return self.safe_execute(self._set_rotate_speed, "Failed to turn off")
+    
+    def _set_rotate_speed(self):
+        return jsonify({"set_rotate_speed": True})
+    
+    # set_wheel_speed
+    def set_wheel_speed(self):
+        return self.safe_execute(self._set_wheel_speed, "Failed to turn off")
+    
+    def _set_wheel_speed(self):
+        return jsonify({"set_pwm_value": True})
+    
+    # move
     def move(self):
         """
         Move the robot.
-
+    
         Returns:
             Response: JSON indicating success.
         """
@@ -147,10 +234,32 @@ class RoboMasterServer:
         self.ep_robot.chassis.move(x=x, y=y, z=z, xy_speed=speed).wait_for_completed()
         return jsonify({"move": True})
     
+    # move_with_time
+    def move_with_time(self):
+        return self.safe_execute(self._move_with_time, "Failed to turn off")
+    
+    def _move_with_time(self):
+        return jsonify({"move_with_time": True})
+    
+    # move_with_distance
+    def move_with_distance(self):
+        return self.safe_execute(self._move_with_distance, "Failed to turn off")
+    
+    def _move_with_distance(self):
+        return jsonify({"move_with_distance": True})
+    
+    # move_degree_with_speed
+    def move_degree_with_speed(self):
+        return self.safe_execute(self._move_degree_with_speed, "Failed to set flash")
+    
+    def _move_degree_with_speed(self):
+        return jsonify({"move_degree_with_speed": True})
+    
+    # rotate
     def rotate(self):
         """
         Rotate the robot.
-
+    
         Returns:
             Response: JSON indicating success.
         """
@@ -165,6 +274,72 @@ class RoboMasterServer:
         speed = float(data.get("speed", 30))
         self.ep_robot.chassis.move(z=angle, xy_speed=speed).wait_for_completed()
         return jsonify({"rotate": True})
+    
+    # rotate_with_time
+    def rotate_with_time(self):
+        return self.safe_execute(self._rotate_with_time, "Failed to set flash")
+    
+    def _rotate_with_time(self):
+        return jsonify({"rotate_with_time": True})
+    
+    # rotate_with_degree
+    def rotate_with_degree(self):
+        return self.safe_execute(self._rotate_with_degree, "Failed to set flash")
+    
+    def _rotate_with_degree(self):
+        return jsonify({"rotate_with_degree": True})
+    
+    # move_and_rotate
+    def move_and_rotate(self):
+        return self.safe_execute(self._move_and_rotate, "Failed to set flash")
+    
+    def _move_and_rotate(self):
+        return jsonify({"move_and_rotate": True})
+    
+    # move_with_speed
+    def move_with_speed(self):
+        return self.safe_execute(self._move_with_speed, "Failed to set flash")
+    
+    def _move_with_speed(self):
+        return jsonify({"move_with_speed": True})
+    
+    # stop
+    def stop(self):
+        return self.safe_execute(self._stop, "Failed to set flash")
+    
+    def _stop(self):
+        return jsonify({"stop": True})
+    
+    # get_attitude
+    def get_attitude(self):
+        return self.safe_execute(self._get_attitude, "Failed to set flash")
+    
+    def _get_attitude(self):
+        return jsonify({"get_attitude": True})
+    
+    # get_position_based_power_on
+    def get_position_based_power_on(self):
+        return self.safe_execute(self._get_position_based_power_on, "Failed to set flash")
+    
+    def _get_position_based_power_on(self):
+        return jsonify({"get_position_based_power_on": True})
+    
+    # chassis_impact_detection
+    def chassis_impact_detection(self):
+        return self.safe_execute(self._chassis_impact_detection, "Failed to set flash")
+    
+    def _chassis_impact_detection(self):
+        return jsonify({"chassis_impact_detection": True})
+    
+    # is_impact
+    def is_impact(self):
+        return self.safe_execute(self._is_impact, "Failed to set flash")
+    
+    def _is_impact(self):
+        return jsonify({"is_impact": True})
+    
+    
+    # Extension Module
     
     def arm(self):
         """
@@ -204,6 +379,98 @@ class RoboMasterServer:
         else:
             self.ep_robot.grabber.close().wait_for_completed()
         return jsonify({"grabber": True})
+    
+    
+    # Smart
+    
+    
+    
+    
+    # Armor
+    
+    # set_hit_sensitivity
+    def set_hit_sensitivity(self):
+        return self.safe_execute(self._set_hit_sensitivity, "Failed to set flash")
+    
+    def _set_hit_sensitivity(self):
+        return jsonify({"set_hit_sensitivity": True})
+    
+    # armor_hit_detection_all
+    def armor_hit_detection_all(self):
+        return self.safe_execute(self._armor_hit_detection_all, "Failed to set flash")
+    
+    def _armor_hit_detection_all(self):
+        return jsonify({"armor_hit_detection_all": True})
+    
+    # get_last_hit_index
+    def get_last_hit_index(self):
+        return self.safe_execute(self._get_last_hit_index, "Failed to set flash")
+    
+    def _get_last_hit_index(self):
+        return jsonify({"get_last_hit_index": True})
+    
+    # check_condition
+    def check_condition(self):
+        return self.safe_execute(self._check_condition, "Failed to set flash")
+    
+    def _check_condition(self):
+        return jsonify({"is_impact": True})
+    
+    # cond_wait
+    def cond_wait(self):
+        return self.safe_execute(self._cond_wait, "Failed to set flash")
+    
+    def _cond_wait(self):
+        return jsonify({"cond_wait": True})
+    
+    
+    # Sensor
+    
+    
+    
+    
+    # Sensor Adapter
+    
+    
+    
+    
+    # Media
+    
+    # play_sound
+    def play_sound(self):
+        return self.safe_execute(self._play_sound, "Failed to set flash")
+    
+    def _play_sound(self):
+        return jsonify({"play_sound": True})
+    
+    # play_sound
+    def play_sound(self):
+        return self.safe_execute(self._play_sound, "Failed to set flash")
+    
+    def _play_sound(self):
+        return jsonify({"play_sound": True})
+    
+    # capture
+    def capture(self):
+        return self.safe_execute(self._capture, "Failed to set flash")
+    
+    def _capture(self):
+        return jsonify({"capture": True})
+    
+    # record
+    def record(self):
+        return self.safe_execute(self._record, "Failed to set flash")
+    
+    def _record(self):
+        return jsonify({"cond_wait": True})
+    
+    # say
+    def say(self):
+        return self.safe_execute(self._say, "Failed to set flash")
+    
+    def _say(self):
+        return jsonify({"say": True})
+    
 
 
 # ==================== if __name__ == '__main__' ==================== #
